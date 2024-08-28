26da7cd3c2ce9e6bcec3e38098212c0c67877ea4
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-08-28 16:43:20 +0200

    Milestone 0

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('JS OK')
+
+// * MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+const ourTeam = [
+        {name: 'Wayne' , lastname: 'Barnett' , job: 'Founder & CEO' , photo: 'wayne-barnett-founder-ceo.jpg'},
+        {name: 'Angela' , lastname: 'Caroll' , job: 'Chief Editor' , photo: 'angela-caroll-chief-editor.jpg'},
+        {name: 'Walter' , lastname: 'Gordon' , job: 'Office Manager' , photo: 'walter-gordon-office-manager.jpg'},
+        {name: 'Angela' , lastname: 'Lopez' , job: 'Social Media Manager' , photo: 'angela-lopez-social-media-manager.jpg'},
+        {name: 'Scott' , lastname: 'Estrada' , job: 'Developer' , photo: 'scott-estrada-developer.jpg'},
+        {name: 'Barbara' , lastname: 'Ramos' , job: 'Graphic Designer' , photo: 'barbara-ramos-graphic-designer.jpg'},
+    ]
